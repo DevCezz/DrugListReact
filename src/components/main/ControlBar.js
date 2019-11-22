a2d9eaca874f2d7a1a13c4947bf80136730a68d9
Cezary Sanecki <csanecki@gmail.com>
2019-11-22 15:38:04 +0100

    added min to inputs

--- src/components/main/ControlBar.js
+++ src/components/main/ControlBar.js
@@ -10,13 +10,13 @@ export const ControlBar = memo(({ showFormAddDrug, filterDrugsByPrice, limitBelo
                 <div className="row m-0">
                     <label className="col-lg-3 col-form-label form-control-label text-right px-3">Cena: </label>
                     <div className="col-lg-4 px-0">
-                        <input className="form-control" type="number" placeholder="Od" 
+                        <input className="form-control" type="number" placeholder="Od" min="0"
                             onChange={ event => filterDrugsByPrice(event.target.value, limitAbove) }
                             onKeyUp={ event => filterDrugsByPrice(event.target.value, limitAbove) } />
                     </div>
                     <label className="col-lg-1 col-form-label text-center px-0"> - </label>
                     <div className="col-lg-4 px-0">
-                        <input className="form-control" type="number" placeholder="Do" 
+                        <input className="form-control" type="number" placeholder="Do" min="0"
                             onChange={ event => filterDrugsByPrice(limitBelow, event.target.value) }
                             onKeyUp={ event => filterDrugsByPrice(limitBelow, event.target.value) } />
                     </div>
